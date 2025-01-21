e486d5d65a767efa26e80e03b15257da8f450b3a
anthonypulupa <anthonypulupa2@gmail.com>
2025-01-21 08:43:54 -0500

    hola

---
+++ Interfazz/src/main/java/ec/edu/espoch/interfazz/Interfazz.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.interfazz;
+
+/**
+ *
+ * @author anthony
+ */
+public class Interfazz {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
